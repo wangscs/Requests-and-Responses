90176cec9eebef3cb0f50ac594aff15e2829e9b0
Okinz <steven14812@gmail.com>
2021-05-26 13:09:33 -0400

    Clean up code aka comments

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -24,20 +24,6 @@ namespace Requests_and_Responses.Controllers
       _logger = logger;
     }
 
-    // [HttpGet]
-    // public IEnumerable<WeatherForecast> Get()
-    // {
-    //     var rng = new Random();
-    //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-    //     {
-    //         Date = DateTime.Now.AddDays(index),
-    //         TemperatureC = rng.Next(-20, 55),
-    //         Summary = Summaries[rng.Next(Summaries.Length)]
-    //     })
-    //     .ToArray();
-    // }
-
-    // [HttpGet("{header}")]
     [HttpGet]
     public IActionResult GetHeader()
     {
